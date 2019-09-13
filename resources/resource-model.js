0afb2e5c16eae5a7727edae69b71d398ef0ca610
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-09-13 11:36:24 -0500

    Completed Resources endpoints

---
+++ resources/resource-model.js
@@ -0,0 +1,19 @@
+const db = require("../data/db-config.js");
+
+module.exports = {
+  getResources,
+  getResourceById,
+  addResource
+};
+
+function getResources() {
+  return db("resources");
+}
+
+function getResourceById(id) {
+  return db("resources").where({ id });
+}
+
+function addResource(resource) {
+  return db("resources").insert(resource);
+}
